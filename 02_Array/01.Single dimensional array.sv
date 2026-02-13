ef56a71f8f155997f8836fef1f1a972b8e85a6cc
mahendiran1202 <mahemaahi07@gmail.com>
2026-02-13 13:15:07 +0530

    Delete 02_Array/01.Single dimensional array.sv

--- 02_Array/01.Single dimensional array.sv
+++
@@ -1,8 +0,0 @@
-module single_dimensional_array;
-  int array[5] = '{14,7,28,42,21};
-  initial begin
-    foreach (array[i]) begin
-      $display("array[%0d] = %0d", i, array[i]);
-    end
-  end
-endmodule
