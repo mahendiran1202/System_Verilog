f404045ede0e2b25fa632c9be383d14e2285bfb5
mahendiran1202 <mahemaahi07@gmail.com>
2026-02-13 13:04:57 +0530

    Delete 02_Array/01.Satic_Array/Two dimensional array.sv

--- 02_Array/01.Satic_Array/Two dimensional array.sv
+++
@@ -1,8 +0,0 @@
-module two_dimensional_array;
-  int array[3][2] = '{'{1, 10}, '{2, 20}, '{3, 30}};
-  initial begin
-    foreach (array[i,j]) begin
-      $display("array[%0d][%0d] = %0d", i,j, array[i][j]);
-    end
-  end
-endmodule
