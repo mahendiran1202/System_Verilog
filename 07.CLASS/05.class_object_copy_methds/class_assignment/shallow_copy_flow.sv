73a29597caf8db864ee489dac63d7e61c332f4c8
MAHENDIRAN S <mahemaahi07@gmail.com>
2026-03-04 21:58:27 +0530

    Create shallow_copy_flow.sv

---
+++ 07.CLASS/05.class_object_copy_methds/class_assignment/shallow_copy_flow.sv
@@ -0,0 +1,42 @@
+class parent;						
+  int a;									
+  function new(int a);
+    this.a = a;
+  endfunction
+endclass
+
+class child;							
+  int b;
+  parent parent_h1;						
+  function new(int a, b);
+    this.b = b;
+    parent_h1 = new(a);					
+  endfunction
+endclass
+
+child child_h1, child_h2;				
+
+module shallow_copy_flaw;
+  
+  initial begin
+    child_h1 = new(10, 20);    			
+    child_h2 = new child_h1;				
+  
+    $display("--------- before h2 handle change ------------");
+    $display("child handle1 child_h1 => parent a = %0d | b = %0d", child_h1.parent_h1.a, child_h1.b);
+    $display("child handle2 child_h2 => parent a = %0d | b = %0d", child_h2.parent_h1.a, child_h2.b);
+  
+  	child_h2.parent_h1.a = 36;			
+  	child_h2.b = 145;
+  
+  	$display("--------- after h2 handle change ------------");
+    $display("child handle1 child_h1 => parent a = %0d | b = %0d", child_h1.parent_h1.a, child_h1.b);
+    $display("child handle2 child_h2 => parent a = %0d | b = %0d", child_h2.parent_h1.a, child_h2.b);
+    
+    child_h1.b = 675;
+    child_h1.parent_h1.a = 1676;
+    $display("--------- after h1 handle change ------------");
+    $display("child handle1 child_h1 => parent a = %0d | b = %0d", child_h1.parent_h1.a, child_h1.b);
+    $display("child handle2 child_h2 => parent a = %0d | b = %0d", child_h2.parent_h1.a, child_h2.b);
+   end
+endmodule
